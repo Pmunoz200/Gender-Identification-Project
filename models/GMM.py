57bd107d40777f433b3c7d59d0d7e423515ddcd8
Pablo Munoz s310308 <s310308@studenti.polito.it>
2023-09-21 14:46:41 +0200

    Fixed and updated the GMM models

--- models/GMM.py
+++ models/GMM.py
@@ -4,24 +4,12 @@ import scipy
 import os
 from tabulate import tabulate
 import sys
+from matplotlib import pyplot as plt
 
 sys.path.append(os.path.abspath("MLandPattern"))
 import MLandPattern as ML
 
-tableGMM = []
-tableDiag = []
-tableTied = []
-headers = [
-    "GMM:[0.1, 0, 0.01]",
-    "GMM:[0.1, 1, 0.01]",
-    "GMM:[0.1, 2, 0.01]",
-    "Diagonal:[0.1, 0, 0.01]",
-    "Diagonal:[0.1, 1, 0.01]",
-    "Diagonal:[0.1, 2, 0.01]",
-    "Tied:[0.1, 0, 0.01]",
-    "Tied:[0.1, 1, 0.01]",
-    "Tied:[0.1, 2, 0.01]",
-]
+
 pi = 0.5
 Cfn = 1
 Cfp = 1
@@ -53,190 +41,84 @@ def split_db(D, L, fraction, seed=0):
 
     return (DTR, LTR), (DTE, LTE)
 
+def call_GMM(attributes, labels, prior_probability, models, k_value=5, m = 0):
+    tableGMM = []
+    total_iter = len(models)
+    perc = 0
+
+    ### ------------- K-FOLD CROSS-VALIDATION AND PCA ---------------------- ####
+    tableGMM.append(["Full"])
+    result_minDCF = []
+    for model in models:
+        ref = model.split(":")
+        mod = ref[0]
+        constrains = eval(ref[1])
+        if not m:
+            [SPost, Predictions, accuracy, _, minDCF] = ML.k_fold(
+            k_value, attributes, labels, prior_probability, model=mod, niter=constrains[1],alpha=constrains[0], psi=constrains[2]
+            )
+        else:
+            [SPost, Predictions, accuracy, _, minDCF] = ML.k_fold(
+            k_value, attributes, labels, prior_probability, model=mod, niter=constrains[1],alpha=constrains[0], psi=constrains[2], PCA_m=m
+            )
+        perc += 1
+        print(f"{round(perc * 100 / total_iter, 2)}%")
+        tableGMM[0].append(minDCF)
+        result_minDCF.append(minDCF)
+    print()
+    cont = 1
+    print("minDCF table")
+    print(tabulate(tableGMM, headers=headers[0:3]))
+    return result_minDCF
+
+def graph_data(raw_results, z_results, model):
+    attribute = {
+    "Raw": raw_results,
+    "Z-Score": z_results
+    }
+
+    maxVal = max(max(attribute["Raw"]), max(attribute["Z-Score"]))
+
+    x = np.arange(len(raw_results))  # the label locations
+    print(x)
+    width = 0.25  # the width of the bars
+    multiplier = 0
+
+    labels = np.power(2, x)
+
+    fig, ax = plt.subplots(layout='constrained')
+
+    for attribute, measurement in attribute.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, label=attribute)
+        ax.bar_label(rects, padding=3)
+        multiplier += 1
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('minDCF')
+    ax.set_xticks(x + width/2, labels)
+    ax.legend(loc='upper left', ncols=3)
+    ax.set_ylim(0, maxVal + 0.1)
+    plt.savefig(f"{os.getcwd()}/Image/GMM-{model}-PCA.png")
+    plt.show()
+
 
 if __name__ == "__main__":
     path = os.path.abspath("data/Train.txt")
     [full_train_att, full_train_label] = load(path)
-
-    total_iter = 37 * len(headers)
-    perc = 0
-
     priorProb = ML.vcol(np.ones(2) * 0.5)
-    psi = 0.01
-    ### ------------- PCA WITH 2/3 SPLIT ---------------------- ####
-    (train_att, train_label), (test_att, test_labels) = ML.split_db(
-        full_train_att, full_train_label, 2 / 3
-    )
-    tableGMM.append(["Full"])
-    tableDiag.append(["Full"])
-    tableTied.append(["Full"])
-
-    for model in headers:
-        ref = model.split(":")
-        mod = ref[0]
-        constrains = eval(ref[1])
-
-        [SPost, Predictions, accuracy] = ML.Generative_models(
-            train_att,
-            train_label,
-            test_att,
-            priorProb,
-            test_labels,
-            mod,
-            niter=constrains[1],
-            alpha=constrains[0],
-            psi=constrains[2],
-        )
-        confusion_matrix = ML.ConfMat(Predictions, test_labels)
-        DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, pi, Cfn, Cfp)
-        (minDCF, FPRlist, FNRlist) = ML.minCostBayes(SPost, test_labels, pi, Cfn, Cfp)
-        if mod == "GMM":
-            tableGMM[0].append([accuracy, DCFnorm, minDCF])
-        elif mod == "Diagonal":
-            tableDiag[0].append([accuracy, DCFnorm, minDCF])
-        elif mod == "Tied":
-            tableTied[0].append([accuracy, DCFnorm, minDCF])
-        perc += 1
-        print(f"{round(perc * 100 / total_iter, 2)}%")
-    print()
-    cont = 1
-    for i in reversed(range(12)):
-        if i < 2:
-            break
-        P, reduced_train = ML.PCA(train_att, i)
-        reduced_test = np.dot(P.T, test_att)
-
-        tableGMM.append([f"PCA {i}"])
-        tableDiag.append([f"PCA {i}"])
-        tableTied.append([f"PCA {i}"])
-        for model in headers:
-            ref = model.split(":")
-            mod = ref[0]
-            constrains = eval(ref[1])
-
-            [SPost, Predictions, accuracy] = ML.Generative_models(
-                reduced_train,
-                train_label,
-                reduced_test,
-                priorProb,
-                test_labels,
-                mod,
-                niter=constrains[1],
-                alpha=constrains[0],
-                psi=constrains[2],
-            )
-            confusion_matrix = ML.ConfMat(Predictions, test_labels)
-            DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, pi, Cfn, Cfp)
-            (minDCF, FPRlist, FNRlist) = ML.minCostBayes(
-                SPost, test_labels, pi, Cfn, Cfp
-            )
-            if mod == "GMM":
-                tableGMM[cont].append([accuracy, DCFnorm, minDCF])
-            elif mod == "Diagonal":
-                tableDiag[cont].append([accuracy, DCFnorm, minDCF])
-            elif mod == "Tied":
-                tableTied[cont].append([accuracy, DCFnorm, minDCF])
-            perc += 1
-            print(f"{round(perc * 100 / total_iter, 2)}%", end="\t")
-        print()
-        cont += 1
-        for j in reversed(range(i)):
-            if j < 2:
-                break
-            tableGMM.append([f"PCA {i} LDA {j}"])
-            tableDiag.append([f"PCA {i} LDA {j}"])
-            tableTied.append([f"PCA {i} LDA {j}"])
-            W, _ = ML.LDA1(reduced_train, train_label, j)
-            LDA_train = np.dot(W.T, reduced_train)
-            LDA_test = np.dot(W.T, reduced_test)
-            for model in headers:
-                ref = model.split(":")
-                mod = ref[0]
-                constrains = eval(ref[1])
-
-                [SPost, Predictions, accuracy] = ML.Generative_models(
-                    LDA_train,
-                    train_label,
-                    LDA_test,
-                    priorProb,
-                    test_labels,
-                    mod,
-                    niter=constrains[1],
-                    alpha=constrains[0],
-                    psi=constrains[2],
-                )
-                confusion_matrix = ML.ConfMat(Predictions, test_labels)
-                DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, pi, Cfn, Cfp)
-                (minDCF, FPRlist, FNRlist) = ML.minCostBayes(
-                    SPost, test_labels, pi, Cfn, Cfp
-                )
-                if mod == "GMM":
-                    tableGMM[cont].append([accuracy, DCFnorm, minDCF])
-                elif mod == "Diagonal":
-                    tableDiag[cont].append([accuracy, DCFnorm, minDCF])
-                elif mod == "Tied":
-                    tableTied[cont].append([accuracy, DCFnorm, minDCF])
-                perc += 1
-                print(f"{round(perc * 100 / total_iter, 2)}%", end="\t")
-            print()
-            cont += 1
-    # headersPCA = []
-    # for i in headers:
-    #     headersPCA.append(i + " Acc/DCF/MinDCF")
-    print("PCA GMM with a 2/3 split")
-    print(tabulate(tableGMM, headers=headers[0:3]))
-    print("PCA Diagonal with a 2/3 split")
-    print(tabulate(tableDiag, headers=headers[3:6]))
-    print("PCA Tied with a 2/3 split")
-    print(tabulate(tableTied, headers=headers[6:]))
-
-    ### ------------- k-fold with different PCA ---------------------- ####
-    # headers = ["MVG", "Naive", "Tied Gaussian", "Tied Naive"]
-    # tableKFold.append(["Full"])
-    # print(f"Size of dataset: {full_train_att.shape[1]}")
-    # # k_fold_value = int(input("Value for k partitions: "))
-    # k_fold_value = 20
-    # for model in headers:
-    #     [SPost, Predictions, accuracy, DCFnorm, minDCF] = ML.k_fold(
-    #         k_fold_value, full_train_att, full_train_label, priorProb, model
-    #     )
-    #     tableKFold[0].append([accuracy, DCFnorm, minDCF])
-
-    # cont = 1
-    # for i in reversed(range(10)):
-    #     if i < 2:
-    #         break
-
-    #     tableKFold.append([f"PCA {i}"])
-    #     for model in headers:
-    #         [SPost, Predictions, accuracy, DCFnorm, minDCF] = ML.k_fold(
-    #             k_fold_value,
-    #             full_train_att,
-    #             full_train_label,
-    #             priorProb,
-    #             model,
-    #             PCA_m=i,
-    #         )
-    #         tableKFold[cont].append([accuracy, DCFnorm, minDCF])
-
-    #     cont += 1
-    #     for j in reversed(range(i)):
-    #         if j < 2:
-    #             break
-    #         tableKFold.append([f"PCA {i} LDA {j}"])
-    #         for model in headers:
-    #             [SPost, Predictions, accuracy, DCFnorm, minDCF] = ML.k_fold(
-    #                 k_fold_value,
-    #                 full_train_att,
-    #                 full_train_label,
-    #                 priorProb,
-    #                 model,
-    #                 PCA_m=i,
-    #                 LDA_m=j,
-    #             )
-    #             tableKFold[cont].append([accuracy, DCFnorm, minDCF])
-
-    #         cont += 1
-
-    # print("PCA with k-fold")
-    # print(tabulate(tableKFold, headers=headers))
+    headers = [
+    "GMM:[0.1, 0, 0.01]",
+    "GMM:[0.1, 1, 0.01]",
+    "GMM:[0.1, 2, 0.01]",
+    "GMM:[0.1, 3, 0.01]",
+    "GMM:[0.1, 4, 0.01]",
+    "GMM:[0.1, 5, 0.01]",
+    ]
+    standard_deviation = np.std(full_train_att)
+    z_data = ML.center_data(full_train_att) / standard_deviation
+    print("Full data")
+    raw_values = call_GMM(full_train_att, full_train_label, priorProb, models=headers, m=12)
+    print("Z-norm data")
+    z_values = call_GMM(z_data, full_train_label, priorProb, models=headers, m=12)
+    graph_data(raw_values, z_values, headers[0][0:3])
