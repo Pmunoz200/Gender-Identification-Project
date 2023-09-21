dc01b2cd7db550f4007cd900e64e4503743fbd05
sromero1220 <saroar98@gmail.com>
2023-09-22 00:41:22 +0200

    regression changes

--- models/regression.py
+++ models/regression.py
@@ -9,19 +9,6 @@ import matplotlib.pyplot as plt
 sys.path.append(os.path.abspath("MLandPattern"))
 import MLandPattern as ML
 
-tablePCA = []
-tableKFold = []
-headers = [
-    "Dimensions",
-    "LR lambda=10^-6 ACC/DCF/minDCF",
-    "LR lambda=10^-3 ACC/DCF/minDCF",
-    "LR lambda=10^-1 ACC/DCF/minDCF",
-    "LR lambda=1 ACC/DCF/minDCF",
-]
-pi = 0.5
-Cfn = 1
-Cfp = 1
-
 
 def load(pathname, vizualization=0):
     df = pd.read_csv(pathname, header=None)
@@ -51,119 +38,23 @@ def split_db(D, L, fraction, seed=0):
 
 
 if __name__ == "__main__":
-    l_list = [10**-6, 10**-3, 10**-1, 1]
+    tableKFold = []
+    pi=[0.5,0.3,0.7]
+    Cfn = 1
+    Cfp = 1
+    l_list = np.logspace(-6, 6, 5)
     path = os.path.abspath("data/Train.txt")
     [full_train_att, full_train_label] = load(path)
-
     priorProb = ML.vcol(np.ones(2) * 0.5)
-
-    q = int(input("Quadratic=1 and logarithmic=0: "))
-
-    # ### ------------- PCA WITH 2/3 SPLIT ---------------------- ####
-
-    (train_att, train_label), (test_att, test_labels) = ML.split_db(
-        full_train_att, full_train_label, 2 / 3
-    )
-
-    tablePCA.append(["Full"])
-
-    for l in l_list:
-        [Predictions, SPost, accuracy] = ML.binaryRegression(
-            train_att, train_label, l, test_att, test_labels, quadratic=q
-        )
-
-        confusion_matrix = ML.ConfMat(Predictions, test_labels)
-        DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, pi, Cfn, Cfp)
-        (minDCF, _, _) = ML.minCostBayes(SPost, test_labels, pi, Cfn, Cfp)
-        tablePCA[0].append([accuracy, DCFnorm, minDCF])
-
-    cont = 1
-    for i in reversed(range(10)):
-        if i < 2:
-            break
-        P, reduced_train = ML.PCA(train_att, i)
-        reduced_test = np.dot(P.T, test_att)
-
-        tablePCA.append([f"PCA {i}"])
-
-        for l in l_list:
-            [_, SPost, accuracy] = ML.binaryRegression(
-                reduced_train, train_label, l, reduced_test, test_labels, quadratic=q
-            )
-            [Predictions, _] = ML.calculate_model(
-                SPost, test_att, "Regression", priorProb, test_labels
-            )
-            confusion_matrix = ML.ConfMat(Predictions, test_labels)
-            DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, pi, Cfn, Cfp)
-            (minDCF, _, _) = ML.minCostBayes(SPost, test_labels, pi, Cfn, Cfp)
-
-            tablePCA[cont].append([accuracy, DCFnorm, minDCF])
-        cont += 1
-        for j in reversed(range(i)):
-            if j < 2:
-                break
-            tablePCA.append([f"PCA {i} LDA {j}"])
-            W, _ = ML.LDA1(reduced_train, train_label, j)
-            LDA_train = np.dot(W.T, reduced_train)
-            LDA_test = np.dot(W.T, reduced_test)
-
-            for l in l_list:
-                [_, SPost, accuracy] = ML.binaryRegression(
-                    LDA_train, train_label, l, LDA_test, test_labels, quadratic=q
-                )
-                [Predictions, _] = ML.calculate_model(
-                    SPost, test_att, "Regression", priorProb, test_labels
-                )
-                confusion_matrix = ML.ConfMat(Predictions, test_labels)
-                DCF, DCFnorm = ML.Bayes_risk(confusion_matrix, pi, Cfn, Cfp)
-                (minDCF, _, _) = ML.minCostBayes(SPost, test_labels, pi, Cfn, Cfp)
-                tablePCA[cont].append([accuracy, DCFnorm, minDCF])
-            cont += 1
-
-    print(f"PCA with a 2/3 split with lambda={l}")
-    print(tabulate(tablePCA, headers=headers))
-
-    ### K-fold binomial Regression ###
-    k = int(input("Number of partitions: "))
-    print(f"Size of dataset: {full_train_att.shape[1]}")
-    tableKFold = []
-    tableKFold.append(["Full"])
-    for l in l_list:
-        [_, _, accuracy, DCFnorm, minDCF] = ML.k_fold(
-            k,
-            full_train_att,
-            full_train_label,
-            priorProb,
-            "regression",
-            l=l,
-            quadratic=q,
-        )
-        tableKFold[0].append([accuracy, DCFnorm, minDCF])
-
-    cont = 1
-    for i in reversed(range(10)):
-        if i < 2:
-            break
-
-        tableKFold.append([f"PCA {i}"])
-        for l in l_list:
-            [_, _, accuracy, DCFnorm, minDCF] = ML.k_fold(
-                k,
-                full_train_att,
-                full_train_label,
-                priorProb,
-                "regression",
-                PCA_m=i,
-                l=l,
-                quadratic=q,
-            )
-            tableKFold[cont].append([accuracy, DCFnorm, minDCF])
-
-        cont += 1
-        for j in reversed(range(i)):
-            if j < 2:
-                break
-            tableKFold.append([f"PCA {i} LDA {j}"])
+    q=[0,1]
+    k=5
+    
+    for i in q:       
+        minDCFvalues5=[]
+        minDCFvalues3=[]
+        minDCFvalues7=[]
+        for p in pi:
+            
             for l in l_list:
                 [_, _, accuracy, DCFnorm, minDCF] = ML.k_fold(
                     k,
@@ -171,13 +62,170 @@ if __name__ == "__main__":
                     full_train_label,
                     priorProb,
                     "regression",
-                    PCA_m=i,
-                    LDA_m=j,
                     l=l,
-                    quadratic=q,
+                    pi=p,
+                    quadratic=i,
+    
                 )
-                tableKFold[cont].append([accuracy, DCFnorm, minDCF])
-            cont += 1
+                if p== 0.5:
+                    minDCFvalues5.append(minDCF)
+                if p== 0.3:
+                    minDCFvalues3.append(minDCF)
+                if p== 0.7:
+                    minDCFvalues7.append(minDCF)
 
-    print(f"PCA with k-fold and lambda: {l}")
-    print(tabulate(tableKFold, headers=headers))
+        if i==1:
+            print(f"Quadratic regression with k-fold = {k}")
+        if i==0:
+            print(f"Logarithmic regression with k-fold = {k}")
+        plt.figure(figsize=(10, 6))
+        plt.semilogx(l_list, minDCFvalues5, label=f'π= 0.5')
+        plt.semilogx(l_list, minDCFvalues3, label=f'π= 0.3')
+        plt.semilogx(l_list, minDCFvalues7, label=f'π= 0.7')
+        plt.xlabel('lambda')
+        plt.ylabel('minDCF')
+        if i==0:
+         plt.title('Logarithmic regression')
+        if i==1:
+         plt.title('Quadratic regression')
+        plt.legend()
+        plt.show()
+        
+    # #Z-norm
+    standard_deviation = np.std(full_train_att)
+    z_data = ML.center_data(full_train_att) / standard_deviation      
+    for i in q:       
+        minDCFvalues5=[]
+        minDCFvalues3=[]
+        minDCFvalues7=[]
+        for p in pi:
+            
+            for l in l_list:
+                [_, _, accuracy, DCFnorm, minDCF] = ML.k_fold(
+                    k,
+                    z_data,
+                    full_train_label,
+                    priorProb,
+                    "regression",
+                    l=l,
+                    pi=p,
+                    quadratic=i,
+    
+                )
+                if p== 0.5:
+                    minDCFvalues5.append(minDCF)
+                if p== 0.3:
+                    minDCFvalues3.append(minDCF)
+                if p== 0.7:
+                    minDCFvalues7.append(minDCF)
+
+        if i==1:
+            print(f"Quadratic regression Z-norm ")
+        if i==0:
+            print(f"Logarithmic regression Z-norm ")
+        plt.figure(figsize=(10, 6))
+        plt.semilogx(l_list, minDCFvalues5, label=f'π= 0.5')
+        plt.semilogx(l_list, minDCFvalues3, label=f'π= 0.3')
+        plt.semilogx(l_list, minDCFvalues7, label=f'π= 0.7')
+        plt.xlabel('lambda')
+        plt.ylabel('minDCF')
+        if i==0:
+         plt.title('Logarithmic regression Z-norm')
+        if i==1:
+         plt.title('Quadratic regression Z-norm')
+        plt.legend()
+        plt.show()
+    for i in q:       
+        minDCFvalues5=[]
+        minDCFvalues3=[]
+        minDCFvalues7=[]
+        for p in pi:
+            
+            for l in l_list:
+                [_, _, accuracy, DCFnorm, minDCF] = ML.k_fold(
+                    k,
+                    full_train_att,
+                    full_train_label,
+                    priorProb,
+                    "regression",
+                    PCA_m=11,
+                    l=l,
+                    pi=p,
+                    quadratic=i,
+    
+                )
+                if p== 0.5:
+                    minDCFvalues5.append(minDCF)
+                if p== 0.3:
+                    minDCFvalues3.append(minDCF)
+                if p== 0.7:
+                    minDCFvalues7.append(minDCF)
+
+        if i==1:
+            print(f"Quadratic regression PCA 11")
+        if i==0:
+            print(f"Logarithmic regression PCA 11 ")
+        plt.figure(figsize=(10, 6))
+        plt.semilogx(l_list, minDCFvalues5, label=f'π= 0.5')
+        plt.semilogx(l_list, minDCFvalues3, label=f'π= 0.3')
+        plt.semilogx(l_list, minDCFvalues7, label=f'π= 0.7')
+        plt.xlabel('lambda')
+        plt.ylabel('minDCF')
+        if i==0:
+         plt.title('Logarithmic regression PCA 11')
+        if i==1:
+         plt.title('Quadratic regression PCA 11')
+        plt.legend()
+        plt.show()  
+    
+      
+    searchL=float(input("Enter a lambda value you want to search for: "))
+    headers = [
+    "Model",
+    "π=0.5 DCF/minDCF",
+    "π=0.3 DCF/minDCF",
+    "π=0.7 DCF/minDCF",
+    "π=0.5 Z-min DCF/minDCF",
+    "π=0.3 Z-min DCF/minDCF",
+    "π=0.7 Z-min DCF/minDCF",
+    ]
+    pit = [0.5,0.3,0.7]
+    cont = 0
+    for i in q:
+        for x in pit:
+            if i==0:
+                tableKFold.append([f"LR {x}"])
+            if i==1:
+                tableKFold.append([f"QLR {x}"])
+            
+            for p in pi:
+                [_, _, accuracy, DCFnorm, minDCF] = ML.k_fold(
+                        k,
+                        full_train_att,
+                        full_train_label,
+                        priorProb,
+                        "regression",
+                        l=searchL,
+                        pi=p,
+                        quadratic=i,
+                        pit=x
+                    )
+                tableKFold[cont].append([DCFnorm, minDCF])
+            for p in pi:
+                [_, _, accuracy, DCFnorm, minDCF] = ML.k_fold(
+                        k,
+                        z_data,
+                        full_train_label,
+                        priorProb,
+                        "regression",
+                        l=searchL,
+                        pi=p,
+                        quadratic=i,
+                        pit=x
+                    )
+                tableKFold[cont].append([DCFnorm, minDCF])
+            cont += 1
+   
+    print(tabulate(tableKFold, headers))
+        
+
