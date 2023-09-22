a13c41fdc37396d42f88df9dabc2eafd4327e6b0
Pablo Munoz s310308 <s310308@studenti.polito.it>
2023-09-22 21:30:09 +0200

    Changed the precision of the SVM method

--- MLandPattern/MLandPattern.py
+++ MLandPattern/MLandPattern.py
@@ -1114,7 +1114,7 @@ def svm(
         alp,
         args=(training_att, training_labels, K, dim, c, eps, gamma, model),
         bounds=constrain,
-        factr=10000   
+        factr=100000000   
     )
     zi = 2 * training_labels - 1
     kern = model.lower()
